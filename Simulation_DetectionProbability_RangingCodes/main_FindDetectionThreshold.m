1c2d94b0388eb61b7531f5ca20b1876e9c1bea1e
ChoiYunSub_CSL <yunsub.kr@gmail.com>
2019-03-24 10:43:44 +0900

    +Additional Return Value(Pfa)
    +Pfa Return
      +revise Function Call
    +Change major index dimension (col -> row)

--- Simulation_DetectionProbability_RangingCodes/main_FindDetectionThreshold.m
+++ Simulation_DetectionProbability_RangingCodes/main_FindDetectionThreshold.m
@@ -11,29 +11,42 @@ load([path,'CorrOut_GALE1.mat']);
 
 %% Vt Finding Loops
 % % 
-CN_dB = [15 20 25 30 35 40 45];
+CN_dB = [15; 20; 25; 30; 35; 40; 45];
 lenLoop = length(CN_dB);
 
 sigma = 1;  % RMS of noise power
 
 % % Vt Finding based on Auto-Correlation Results
-Vt_GPSL1CA_A = zeros(1, lenLoop);
-Vt_GPSL1Cp_A = zeros(1, lenLoop);
-Vt_GPSL1Cd_A = zeros(1, lenLoop);
-Vt_BDSB1Cp_A = zeros(1, lenLoop);
-Vt_BDSB1Cd_A = zeros(1, lenLoop);
-Vt_GALE1b_A = zeros(1, lenLoop);
-Vt_GALE1c_A = zeros(1, lenLoop);
+Vt_GPSL1CA_A = zeros(lenLoop, 2);
+Vt_GPSL1Cp_A = zeros(lenLoop, 2);
+Vt_GPSL1Cd_A = zeros(lenLoop, 2);
+Vt_BDSB1Cp_A = zeros(lenLoop, 2);
+Vt_BDSB1Cd_A = zeros(lenLoop, 2);
+Vt_GALE1b_A =  zeros(lenLoop, 2);
+Vt_GALE1c_A =  zeros(lenLoop, 2);
 
 for idx = 1:lenLoop
-    Vt_GPSL1CA_A(idx) = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 32, CorrOut_GPSL1CA,  1023, sigma, 3.03,0.01);
-    Vt_GPSL1Cp_A(idx) = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 63, CorrOut_GPSL1Cp, 10230, sigma, 3.03,0.01);
-    Vt_GPSL1Cd_A(idx) = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 63, CorrOut_GPSL1Cd, 10230, sigma, 3.03,0.01);
-    Vt_BDSB1Cp_A(idx) = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 63, CorrOut_BDSB1Cp, 10230, sigma, 3.03,0.01);
-    Vt_BDSB1Cd_A(idx) = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 63, CorrOut_BDSB1Cd, 10230, sigma, 3.03,0.01);
-    Vt_GALE1b_A(idx)  = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 50,  CorrOut_GALE1b,  4092, sigma, 3.03,0.01);
-    Vt_GALE1c_A(idx)  = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 50,  CorrOut_GALE1c,  4092, sigma, 3.03,0.01);
-
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 32, CorrOut_GPSL1CA,  1023, sigma, 3.03,0.01);
+    Vt_GPSL1CA_A(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 63, CorrOut_GPSL1Cp, 10230, sigma, 3.03,0.01);
+    Vt_GPSL1Cp_A(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 63, CorrOut_GPSL1Cd, 10230, sigma, 3.03,0.01);
+    Vt_GPSL1Cd_A(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 63, CorrOut_BDSB1Cp, 10230, sigma, 3.03,0.01);
+    Vt_BDSB1Cp_A(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 63, CorrOut_BDSB1Cd, 10230, sigma, 3.03,0.01);
+    Vt_BDSB1Cd_A(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 50,  CorrOut_GALE1b,  4092, sigma, 3.03,0.01);
+    Vt_GALE1b_A(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Auto',1e-10, CN_dB(idx), 50,  CorrOut_GALE1c,  4092, sigma, 3.03,0.01);
+    Vt_GALE1c_A(idx,:) = [vt, pfa];
+    
     save([path,'Vt_Auto.mat'],...
         'Vt_GPSL1CA_A',...
         'Vt_GPSL1Cp_A',...
@@ -49,23 +62,36 @@ end
 
 
 % % Vt Finding based on Auto-Correlation Results
-Vt_GPSL1CA_C = zeros(1, lenLoop);
-Vt_GPSL1Cp_C = zeros(1, lenLoop);
-Vt_GPSL1Cd_C = zeros(1, lenLoop);
-Vt_BDSB1Cp_C = zeros(1, lenLoop);
-Vt_BDSB1Cd_C = zeros(1, lenLoop);
-Vt_GALE1b_C = zeros(1, lenLoop);
-Vt_GALE1c_C = zeros(1, lenLoop);
+Vt_GPSL1CA_C = zeros(lenLoop, 2);
+Vt_GPSL1Cp_C = zeros(lenLoop, 2);
+Vt_GPSL1Cd_C = zeros(lenLoop, 2);
+Vt_BDSB1Cp_C = zeros(lenLoop, 2);
+Vt_BDSB1Cd_C = zeros(lenLoop, 2);
+Vt_GALE1b_C =  zeros(lenLoop, 2);
+Vt_GALE1c_C =  zeros(lenLoop, 2);
 
 for idx = 1:lenLoop
-    Vt_GPSL1CA_C(idx) = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 32, CorrOut_GPSL1CA,  1023, sigma, 3.03,0.01);
-    Vt_GPSL1Cp_C(idx) = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 63, CorrOut_GPSL1Cp, 10230, sigma, 3.03,0.01);
-    Vt_GPSL1Cd_C(idx) = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 63, CorrOut_GPSL1Cd, 10230, sigma, 3.03,0.01);
-    Vt_BDSB1Cp_C(idx) = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 63, CorrOut_BDSB1Cp, 10230, sigma, 3.03,0.01);
-    Vt_BDSB1Cd_C(idx) = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 63, CorrOut_BDSB1Cd, 10230, sigma, 3.03,0.01);
-    Vt_GALE1b_C(idx)  = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 50,  CorrOut_GALE1b,  4092, sigma, 3.03,0.01);
-    Vt_GALE1c_C(idx)  = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 50,  CorrOut_GALE1c,  4092, sigma, 3.03,0.01);
-
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 32, CorrOut_GPSL1CA,  1023, sigma, 3.03,0.01);
+    Vt_GPSL1CA_C(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 63, CorrOut_GPSL1Cp, 10230, sigma, 3.03,0.01);
+    Vt_GPSL1Cp_C(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 63, CorrOut_GPSL1Cd, 10230, sigma, 3.03,0.01);
+    Vt_GPSL1Cd_C(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 63, CorrOut_BDSB1Cp, 10230, sigma, 3.03,0.01);
+    Vt_BDSB1Cp_C(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 63, CorrOut_BDSB1Cd, 10230, sigma, 3.03,0.01);
+    Vt_BDSB1Cd_C(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 50,  CorrOut_GALE1b,  4092, sigma, 3.03,0.01);
+    Vt_GALE1b_C(idx,:) = [vt, pfa];
+    
+    [vt, pfa] = fFindDetectionThreshold_Pfa1percent('Cross',1e-10, CN_dB(idx), 50,  CorrOut_GALE1c,  4092, sigma, 3.03,0.01);
+    Vt_GALE1c_C(idx,:) = [vt, pfa];
+    
     save([path,'Vt_Cross.mat'],...
         'Vt_GPSL1CA_C',...
         'Vt_GPSL1Cp_C',...
