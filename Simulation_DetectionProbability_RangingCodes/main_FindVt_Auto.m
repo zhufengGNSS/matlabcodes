f5dc48301b4e46d993738b73c696fab0c673fb44
ChoiYunSub <yunsub.kr@gmail.com>
2019-04-12 22:51:36 +0900

    Vt Find Based on CNR

--- Simulation_DetectionProbability_RangingCodes/main_FindVt_Auto.m
+++ Simulation_DetectionProbability_RangingCodes/main_FindVt_Auto.m
@@ -5,8 +5,8 @@ path = 'D:\TemporalDataStorage\';
 
 load([path,'CorrOut_GPSL1CA.mat']);
 load([path,'CorrOut_GALE1.mat']);
-% load([path,'CorrOut_GPSL1C.mat']);
-% load([path,'CorrOut_BDSB1C.mat']);
+load([path,'CorrOut_GPSL1C.mat']);
+load([path,'CorrOut_BDSB1C.mat']);
 
 %% Common Variables
 CN0_dB = [20 35 50];
@@ -16,11 +16,11 @@ CNR_dB = CN0_dB - 60;
 sigma = 1;  % RMS of noise power
 
 %% Vt Finding Loops @ Sampling Rate 20 MHz
+    fs = 20e6;
     %% Distribution Display @ sampling rate 20 MHz
     addpath('..\genRangingCodes');
     load CodeSequence_PRN_GPSL1CA.mat;
     time = 10000; % [ms]
-    fs = 20e6;
     fc = 1575.42e6;
     
     SampledCode_GPSL1CA = zeros(fs*1e-3,1);
@@ -69,10 +69,10 @@ sigma = 1;  % RMS of noise power
 Vt_GPSL1CA_A = zeros(lenLoop, 4);
 Vt_GALE1b_A =  zeros(lenLoop, 4);
 Vt_GALE1c_A =  zeros(lenLoop, 4);
-% Vt_GPSL1Cp_A = zeros(lenLoop, 4);
-% Vt_GPSL1Cd_A = zeros(lenLoop, 4);
-% Vt_BDSB1Cp_A = zeros(lenLoop, 4);
-% Vt_BDSB1Cd_A = zeros(lenLoop, 4);
+Vt_GPSL1Cp_A = zeros(lenLoop, 4);
+Vt_GPSL1Cd_A = zeros(lenLoop, 4);
+Vt_BDSB1Cp_A = zeros(lenLoop, 4);
+Vt_BDSB1Cd_A = zeros(lenLoop, 4);
 
 prec = 1e-10;
 for idx = 1:lenLoop
@@ -91,95 +91,96 @@ for idx = 1:lenLoop
     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 50, CorrOut_GALE1c,  4092, sigma, vt, prec*10);
     Vt_GALE1c_A(idx,3:4) = [vt, pfa];
     
-%     [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 63, CorrOut_GPSL1Cp, 10230, sigma, 0, 10000);
-%     Vt_GPSL1Cp_A(idx,1:2) = [vt, pfa];
-%     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 63, CorrOut_GPSL1Cp,  10230, sigma, vt, prec*10);
-%     Vt_GPSL1Cp_A(idx,3:4) = [vt, pfa];
-%     
-%     [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 63, CorrOut_GPSL1Cd, 10230, sigma, 0, 10000);
-%     Vt_GPSL1Cd_A(idx,1:2) = [vt, pfa];
-%     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 63, CorrOut_GPSL1Cd,  10230, sigma, vt, prec*10);
-%     Vt_GPSL1Cd_A(idx,3:4) = [vt, pfa];
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 63, CorrOut_GPSL1Cp, 10230, sigma, 0, 10000);
+    Vt_GPSL1Cp_A(idx,1:2) = [vt, pfa];
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 63, CorrOut_GPSL1Cp,  10230, sigma, vt, prec*10);
+    Vt_GPSL1Cp_A(idx,3:4) = [vt, pfa];
     
-%     [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 63, CorrOut_BDSB1Cp, 10230, sigma, 0, 10000);
-%     Vt_BDSB1Cp_A(idx,1:2) = [vt, pfa];
-%     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 63, CorrOut_BDSB1Cp,  10230, sigma, vt, prec*10);
-%     Vt_BDSB1Cp_A(idx,3:4) = [vt, pfa];
-%     
-%     [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 63, CorrOut_BDSB1Cd, 10230, sigma, 0, 10000);
-%     Vt_BDSB1Cd_A(idx,1:2) = [vt, pfa];
-%     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 63, CorrOut_BDSB1Cd,  10230, sigma, vt, prec*10);
-%     Vt_BDSB1Cd_A(idx,3:4) = [vt, pfa];
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 63, CorrOut_GPSL1Cd, 10230, sigma, 0, 10000);
+    Vt_GPSL1Cd_A(idx,1:2) = [vt, pfa];
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 63, CorrOut_GPSL1Cd,  10230, sigma, vt, prec*10);
+    Vt_GPSL1Cd_A(idx,3:4) = [vt, pfa];
     
-    save([path,'Vt_Auto_fs20MHz_gpsCAgalE1_50dB.mat'],...
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 63, CorrOut_BDSB1Cp, 10230, sigma, 0, 10000);
+    Vt_BDSB1Cp_A(idx,1:2) = [vt, pfa];
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 63, CorrOut_BDSB1Cp,  10230, sigma, vt, prec*10);
+    Vt_BDSB1Cp_A(idx,3:4) = [vt, pfa];
+    
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 63, CorrOut_BDSB1Cd, 10230, sigma, 0, 10000);
+    Vt_BDSB1Cd_A(idx,1:2) = [vt, pfa];
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 63, CorrOut_BDSB1Cd,  10230, sigma, vt, prec*10);
+    Vt_BDSB1Cd_A(idx,3:4) = [vt, pfa];
+    
+    save([path,'Vt_Auto_fs20MHz_gpsCAgalE1.mat'],...
         'Vt_GPSL1CA_A',...
+        'Vt_GPSL1Cp_A',...
+        'Vt_GPSL1Cd_A',...
+        'Vt_BDSB1Cp_A',...
+        'Vt_BDSB1Cd_A',...
         'Vt_GALE1b_A',...
         'Vt_GALE1c_A');
-%         'Vt_GPSL1Cp_A',...
-%         'Vt_GPSL1Cd_A',...
-%         'Vt_BDSB1Cp_A',...
-%         'Vt_BDSB1Cd_A',...
     
     disp(['Complete CN0_dB ',num2str(CN0_dB(idx))]);
 end
 
 
+
+%% Vt Finding Loops @ Sampling Rate 5 MHz
 % % Vt Finding based on Auto-Correlation Results
 Vt_GPSL1CA_A = zeros(lenLoop, 4);
 Vt_GALE1b_A =  zeros(lenLoop, 4);
 Vt_GALE1c_A =  zeros(lenLoop, 4);
-% Vt_GPSL1Cp_A = zeros(lenLoop, 4);
-% Vt_GPSL1Cd_A = zeros(lenLoop, 4);
-% Vt_BDSB1Cp_A = zeros(lenLoop, 4);
-% Vt_BDSB1Cd_A = zeros(lenLoop, 4);
+Vt_GPSL1Cp_A = zeros(lenLoop, 4);
+Vt_GPSL1Cd_A = zeros(lenLoop, 4);
+Vt_BDSB1Cp_A = zeros(lenLoop, 4);
+Vt_BDSB1Cd_A = zeros(lenLoop, 4);
 
-%% Vt Finding Loops @ Sampling Rate 5 MHz
 fs = 5e6;
 prec = 1e-10;
 for idx = 1:lenLoop
-    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 32, CorrOut_GPSL1CA,  1023, sigma, 0, 10000);
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 32, CorrOut_GPSL1CA,  1023, sigma, 0, 10000);
     Vt_GPSL1CA_A(idx,1:2) = [vt, pfa];
-    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 32, CorrOut_GPSL1CA,  1023, sigma, vt, prec*10);
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 32, CorrOut_GPSL1CA,  1023, sigma, vt, prec*10);
     Vt_GPSL1CA_A(idx,3:4) = [vt, pfa];
     
-    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 50,  CorrOut_GALE1b,  4092, sigma, 0, 10000);
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 50,  CorrOut_GALE1b,  4092, sigma, 0, 10000);
     Vt_GALE1b_A(idx,1:2) = [vt, pfa];
-    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 50, CorrOut_GALE1b,  4092, sigma, vt, prec*10);
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 50, CorrOut_GALE1b,  4092, sigma, vt, prec*10);
     Vt_GALE1b_A(idx,3:4) = [vt, pfa];
     
-    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 50,  CorrOut_GALE1c,  4092, sigma, 0, 10000);
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 50,  CorrOut_GALE1c,  4092, sigma, 0, 10000);
     Vt_GALE1c_A(idx,1:2) = [vt, pfa];
-    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 50, CorrOut_GALE1c,  4092, sigma, vt, prec*10);
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 50, CorrOut_GALE1c,  4092, sigma, vt, prec*10);
     Vt_GALE1c_A(idx,3:4) = [vt, pfa];
     
-%     [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 63, CorrOut_GPSL1Cp, 10230, sigma, 0, 10000);
-%     Vt_GPSL1Cp_A(idx,1:2) = [vt, pfa];
-%     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 63, CorrOut_GPSL1Cp,  10230, sigma, vt, prec*10);
-%     Vt_GPSL1Cp_A(idx,3:4) = [vt, pfa];
-%     
-%     [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 63, CorrOut_GPSL1Cd, 10230, sigma, 0, 10000);
-%     Vt_GPSL1Cd_A(idx,1:2) = [vt, pfa];
-%     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 63, CorrOut_GPSL1Cd,  10230, sigma, vt, prec*10);
-%     Vt_GPSL1Cp_A(idx,3:4) = [vt, pfa];
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 63, CorrOut_GPSL1Cp, 10230, sigma, 0, 10000);
+    Vt_GPSL1Cp_A(idx,1:2) = [vt, pfa];
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 63, CorrOut_GPSL1Cp,  10230, sigma, vt, prec*10);
+    Vt_GPSL1Cp_A(idx,3:4) = [vt, pfa];
     
-%     [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 63, CorrOut_BDSB1Cp, 10230, sigma, 0, 10000);
-%     Vt_BDSB1Cp_A(idx,1:2) = [vt, pfa];
-%     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 63, CorrOut_BDSB1Cp,  10230, sigma, vt, prec*10);
-%     Vt_BDSB1Cp_A(idx,3:4) = [vt, pfa];
-%     
-%     [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CN0_dB(idx), 63, CorrOut_BDSB1Cd, 10230, sigma, 0, 10000);
-%     Vt_BDSB1Cd_A(idx,1:2) = [vt, pfa];
-%     [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CN0_dB(idx), 63, CorrOut_BDSB1Cd,  10230, sigma, vt, prec*10);
-%     Vt_BDSB1Cd_A(idx,3:4) = [vt, pfa];
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 63, CorrOut_GPSL1Cd, 10230, sigma, 0, 10000);
+    Vt_GPSL1Cd_A(idx,1:2) = [vt, pfa];
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 63, CorrOut_GPSL1Cd,  10230, sigma, vt, prec*10);
+    Vt_GPSL1Cp_A(idx,3:4) = [vt, pfa];
     
-    save([path,'Vt_Auto_fs5MHz_gpsCAgalE1_50dB.mat'],...
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 63, CorrOut_BDSB1Cp, 10230, sigma, 0, 10000);
+    Vt_BDSB1Cp_A(idx,1:2) = [vt, pfa];
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 63, CorrOut_BDSB1Cp,  10230, sigma, vt, prec*10);
+    Vt_BDSB1Cp_A(idx,3:4) = [vt, pfa];
+    
+    [vt, pfa] = fFindVt_BinarySearch('Auto', fs, 0.01, prec, CNR_dB(idx), 63, CorrOut_BDSB1Cd, 10230, sigma, 0, 10000);
+    Vt_BDSB1Cd_A(idx,1:2) = [vt, pfa];
+    [vt, pfa] = fFindVt_DecimalScan('Auto', fs, 0.01, prec/10, CNR_dB(idx), 63, CorrOut_BDSB1Cd,  10230, sigma, vt, prec*10);
+    Vt_BDSB1Cd_A(idx,3:4) = [vt, pfa];
+    
+    save([path,'Vt_Auto_fs5MHz_gpsCAgalE1.mat'],...
         'Vt_GPSL1CA_A',...
+        'Vt_GPSL1Cp_A',...
+        'Vt_GPSL1Cd_A',...
+        'Vt_BDSB1Cp_A',...
+        'Vt_BDSB1Cd_A',...
         'Vt_GALE1b_A',...
         'Vt_GALE1c_A');
-%         'Vt_GPSL1Cp_A',...
-%         'Vt_GPSL1Cd_A',...
-%         'Vt_BDSB1Cp_A',...
-%         'Vt_BDSB1Cd_A',...
     
     disp(['Complete CN0_dB ',num2str(CN0_dB(idx))]);
 end
